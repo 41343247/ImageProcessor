d915576f282ee6178c3a354bdc80ea6da70bbfbd
41343247 <41343247@nfu.edu.tw>
2025-12-15 15:47:20 +0800

    範例完成

--- imageprocessor.h
+++ imageprocessor.h
@@ -2,6 +2,11 @@
 #define IMAGEPROCESSOR_H
 
 #include <QMainWindow>
+#include <QAction>
+#include <QMenu>
+#include <QToolBar>
+#include <QImage>
+#include <QLabel>
 
 class ImageProcessor : public QMainWindow
 {
@@ -10,5 +15,28 @@ class ImageProcessor : public QMainWindow
 public:
     ImageProcessor(QWidget *parent = nullptr);
     ~ImageProcessor();
+
+    void createActions();
+    void createMenus();
+    void createToolBars();
+    void loadFile(QString filename);
+
+private slots:
+    void showOpenFile();
+    void bigFile();
+    void smallFile();
+
+private:
+    QWidget     *central;
+    QMenu       *fileMenu;
+    QToolBar    *fileTool;
+    QImage      img;
+    QString     filename;
+    QLabel      *imgWin;
+    QAction     *openFileAction;
+    QAction     *exitAction;
+
+    QAction     *big;
+    QAction     *small;
 };
 #endif // IMAGEPROCESSOR_H
